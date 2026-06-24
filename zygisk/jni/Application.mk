2b378435d0ae1dd382a35b0cd6d5dd47c506c1bd
uruhara91 <79046614+uruhara91@users.noreply.github.com>
2026-06-24 19:47:49 +0700

    Update Application.mk
    Removed APP_OPTIM setting from Application.mk

--- zygisk/jni/Application.mk
+++ zygisk/jni/Application.mk
@@ -1,5 +1,4 @@
 APP_ABI := arm64-v8a
 APP_PLATFORM := android-31
 APP_STL := none
-APP_OPTIM := release
-APP_CPPFLAGS := -march=armv8.2-a+lse+rcpc -mtune=cortex-a78 -fPIC
+APP_CPPFLAGS := -march=armv8.2-a+lse+rcpc -mtune=cortex-a78
